0a5eda5182a13524e8bd2e387042929fb7bc460d
Sean <napolisc10@gmail.com>
2023-01-13 21:43:09 -0700

    Added basic fetch request to the API link I am using.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function cooper(cooper){
-    console.log(cooper)
-}
+
+fetch('https://api.teleport.org/api/cities')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+
