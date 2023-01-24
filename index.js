a6dcd127406db7118c8b01b6849646448ff509a5
Sean <napolisc10@gmail.com>
2023-01-24 15:19:48 -0700

    Created an ability to find if the inputted city is valid in the API.

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 
-fetch('https://api.teleport.org/api/cities')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
 
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
   searchButton.addEventListener("click", () => {
-    //let cityName = cityInput.value;
-    //let finalURL = `https://api.teleport.org/api/cities`;
+    let cityName = cityInput.value;
+    let finalURL = `https://api.teleport.org/api/cities/?search=${cityName}`;
+    
+    fetch(finalURL)
+    .then(response => response.json())
+    .then(data => {
+        if(data._embedded){
+            let city = data._embedded["city:search-results"][0]
+            console.log(city)
+        }else{
+            console.log("City not found")
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error)
   });
 
 
-
+});
