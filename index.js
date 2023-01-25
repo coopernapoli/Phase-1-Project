7da1c99e9605599cdb136b722b22dbb4a67ee54d
Sean <napolisc10@gmail.com>
2023-01-25 14:49:13 -0700

    Utilizing innerHTML to create a new division that will return population data.

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
-  
+  let populationDivision = document.createElement("div");
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value;
     let finalURL = `https://api.teleport.org/api/urban_areas/?embed=${citySlug}/details`;
-
+populationDivision.innerHTML = "";
     fetch(finalURL)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      let populationData=data.population;
+      populationDivision.innerHTML = populationData;
+      document.body.appendChild(populationDivision);
     })
     .catch(error => {
       console.error('Error:', error)
