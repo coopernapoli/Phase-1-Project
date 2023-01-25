858c1332ea4ef2ced6bd2ba729a2f3b4b2177382
Sean <napolisc10@gmail.com>
2023-01-25 16:09:35 -0700

    More adjusting to integrate with updated index.html

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
-  let populationContainer = document.getElementById("population-container")
-  populationContainer.innerHTML = populationData;
+  let populationContainer = document.getElementById("populationContainer");
+  
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
     let finalURL = `https://api.teleport.org/api/urban_areas/slug:${citySlug}/details`;
@@ -11,9 +11,9 @@ populationDivision.innerHTML = "";
     fetch(finalURL)
     .then(response => response.json())
     .then(data => {
-    let populationData = data.categories.find(category => category.id === "CITY-SIZE").data.find(data => data.id === "POPULATION-SIZE").float_value;;
-      populationDivision.innerHTML = populationData;
-      document.body.appendChild(populationDivision);
+    let populationData = data.categories.find(category => category.id === "CITY-SIZE").data.find(data => data.id === "POPULATION-SIZE").float_value;
+      populationContainer.textContent = populationData;
+
       console.log(data)
     })
     .catch(error => {
