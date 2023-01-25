db06c41f908ae052e25de4d6ca0e36496dcfc1f1
Sean <napolisc10@gmail.com>
2023-01-25 14:41:55 -0700

    Lots of retooling of the API to extract the details portion based off of an embedded citySlug name as opposed to the slug itself.

--- index.js
+++ index.js
@@ -2,23 +2,21 @@
 
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
+  
   searchButton.addEventListener("click", () => {
-    let cityName = cityInput.value;
-    let finalURL = `https://api.teleport.org/api/cities/?search=${cityName}`;
-    
+    let citySlug = cityInput.value;
+    let finalURL = `https://api.teleport.org/api/urban_areas/?embed=${citySlug}/details`;
+
     fetch(finalURL)
     .then(response => response.json())
     .then(data => {
-        if(data._embedded){
-            let city = data._embedded["city:search-results"][0]
-            console.log(city)
-        }else{
-            console.log("City not found")
-        }
+      console.log(data);
     })
     .catch(error => {
-        console.error('Error:', error)
+      console.error('Error:', error)
+    });
   });
+  
+  
 
 
-});
