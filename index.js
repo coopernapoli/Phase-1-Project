26b53d680a79d70360569e23b725d18dd4ce37b8
Sean <napolisc10@gmail.com>
2023-01-25 17:29:31 -0700

    multiplying the float value by 1,000,000 so we can have a better UI/UX when reading the data

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
   let populationContainer = document.getElementById("populationContainer");
+  let populationDivision = document.getElementById("")
   
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
     let finalURL = `https://api.teleport.org/api/urban_areas/slug:${citySlug}/details`;
-populationDivision.innerHTML = "";
+populationContainer.innerHTML = "";
     fetch(finalURL)
     .then(response => response.json())
     .then(data => {
-    let populationData = data.categories.find(category => category.id === "CITY-SIZE").data.find(data => data.id === "POPULATION-SIZE").float_value;
+    let populationData = data.categories.find(category => category.id === "CITY-SIZE").data.find(data => data.id === "POPULATION-SIZE").float_value * 1000000;
       populationContainer.textContent = populationData;
-
       console.log(data)
     })
     .catch(error => {
