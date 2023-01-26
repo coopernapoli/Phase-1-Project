f2d566fe13a84e4d0cb4209a872d271dbd7307ec
Sean <napolisc10@gmail.com>
2023-01-25 17:51:47 -0700

    Built a functionality into the response where if we get a 404 code, we get a DNE return

--- index.js
+++ index.js
@@ -3,24 +3,40 @@
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
   let populationContainer = document.getElementById("populationContainer");
-  let populationDivision = document.getElementById("")
+  let populationDivision = document.getElementById("populationDivision");
+
+  populationContainer.style.display="none";
   
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
     let finalURL = `https://api.teleport.org/api/urban_areas/slug:${citySlug}/details`;
 populationContainer.innerHTML = "";
     fetch(finalURL)
-    .then(response => response.json())
+    .then(response => {
+        if (response.status === 404) {
+            populationContainer.innerHTML = "Urban Area does not exist";
+        return;
+      }
+      return response.json();
+    })
+    
+        
     .then(data => {
-    let populationData = data.categories.find(category => category.id === "CITY-SIZE").data.find(data => data.id === "POPULATION-SIZE").float_value * 1000000;
-      populationContainer.textContent = populationData;
-      console.log(data)
+
+         let populationData = data.categories.find(category => category.id === "CITY-SIZE")
+            .data.find(data => data.id === "POPULATION-SIZE").float_value * 1000000;
+
+            populationContainer.textContent = populationData;
+
+            populationContainer.style.display = "block";
+            console.log(data)
+            
     })
     .catch(error => {
       console.error('Error:', error)
     });
-  });
+
   
   
 
-
+});
