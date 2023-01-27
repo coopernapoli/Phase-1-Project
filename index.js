e59e5ad3c7a73e72bf844571428c1aef7a468502
Sean <napolisc10@gmail.com>
2023-01-27 09:09:53 -0700

    recreating basic template using the population data skeleton for the Startup Salary information.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
   let cityInput = document.getElementById("city-input");
   let populationContainer = document.getElementById("populationContainer");
   let headerTwo = document.getElementById("headerTwo");
+  let startupSalaryContainer = document.getElementById("startupSalaryContainer")
   populationContainer.style.display = "none";
+  startupSalaryContainer.style.display = "none";l
 
   let populationDivision = document.getElementById("populationDivision");
+  let startupSalaryDivision = document.getElementById("startupSalaryDivision");
 
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
@@ -42,6 +45,33 @@
     });
 
   
-  
+    startupSalaryContainer.innerHTML = "";
+
+    fetch(finalURL)
+    .then(response => {
+        if (response.status === 404) {
+            startupSalaryContainer.innerHTML = "Urban Area does not exist";
+        return;
+      }
+      return response.json();
+    })
+    
+        
+    .then(data => {
+        
+         let startupSalaryData = data.categories.find(category => category.id === "CITY-SIZE")
+            .data.find(data => data.id === "POPULATION-SIZE").float_value;
+
+            startupSalaryContainer.textContent = startupSalaryData;
+            
+            startupSalaryContainer.style.display = "block";
+        
+        
+            console.log(data)
+            
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    });
 
 });
