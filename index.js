55817872a384dc5117b3cd0deeba366e916344fe
Sean <napolisc10@gmail.com>
2023-01-26 17:39:17 -0700

    Playing with small tweaks, non-important.

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
   let populationContainer = document.getElementById("populationContainer");
+  let headerTwo = document.getElementById("headerTwo");
+  populationContainer.style.display = "none";
+
   let populationDivision = document.getElementById("populationDivision");
 
-  populationContainer.style.display="none";
-  
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
     let finalURL = `https://api.teleport.org/api/urban_areas/slug:${citySlug}/details`;
-populationContainer.innerHTML = "";
+    
+    populationContainer.innerHTML = "";
+
     fetch(finalURL)
     .then(response => {
         if (response.status === 404) {
@@ -22,13 +25,15 @@ populationContainer.innerHTML = "";
     
         
     .then(data => {
-
+        
          let populationData = data.categories.find(category => category.id === "CITY-SIZE")
             .data.find(data => data.id === "POPULATION-SIZE").float_value * 1000000;
 
             populationContainer.textContent = populationData;
-
+            
             populationContainer.style.display = "block";
+        
+        
             console.log(data)
             
     })
