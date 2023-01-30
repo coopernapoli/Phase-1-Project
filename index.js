1f8e7e9d731fef62fbf2f1a707858dfae4e836db
Sean <napolisc10@gmail.com>
2023-01-30 13:04:05 -0700

    Commented the process and decision making.

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-
+//Starting with variable declaration to initialize manipulatable HTML elements.
 
   let searchButton = document.getElementById("search");
   let cityInput = document.getElementById("city-input");
   let populationContainer = document.getElementById("populationContainer");
   let headerTwo = document.getElementById("headerTwo");
-  let startupSalaryContainer = document.getElementById("startupSalaryContainer")
+  let startupSalaryContainer = document.getElementById("startupSalaryContainer");
+
+  //Allows the container elements to only display when called by their respective functions, not before.
   populationContainer.style.display = "none";
   startupSalaryContainer.style.display = "none";
+
+//Likely will not need to use them, but may need these divisions for styling or informational purposes.
 
   let populationDivision = document.getElementById("populationDivision");
   let startupSalaryDivision = document.getElementById("startupSalaryDivision");
 
+//Created the search button event listener. Utilized .toLowerCase() as this was the only way to work with the slug functionality on the API.
+
   searchButton.addEventListener("click", () => {
     let citySlug = cityInput.value.toLowerCase();
     let finalURL = `https://api.teleport.org/api/urban_areas/slug:${citySlug}/details`;
-    
+
+//This allows for multiple intputs to the container with a repopulation of new data, as oppsed to stacking each search on top of each other.
     populationContainer.innerHTML = "";
 
+
+//Created a fetch function to access the API, create conditional responses based on whether or not the city exists, and return a float value of the 
+//population size multiplied by one million so as to provide an acurate, easily readable number.
     fetch(finalURL)
     .then(response => {
         if (response.status === 404) {
@@ -44,9 +54,11 @@
       console.error('Error:', error)
     });
 
-  
+//This allows for multiple intputs to the container with a repopulation of new data, as oppsed to stacking each search on top of each other.
     startupSalaryContainer.innerHTML = "";
 
+//Created a fetch request to access a different part of the API using a similar template as above. This calculates average salary data in the region
+//and it uses the native currency.
     fetch(finalURL)
     .then(response => {
         if (response.status === 404) {
@@ -75,7 +87,9 @@
       console.error('Error:', error)
     });
 
-
+//With crime data being difficult to explain numerically, I created arbitrary value windows to return a few conditional returns after filtering over
+// the float value of the crime data.  These numbers may not necessarily be indicative of what actually constitutes the return strings, but for the sake
+// of the exercise, it was done in this manner.
     fetch(finalURL)
     .then(response => {
         if (response.status === 404) {
